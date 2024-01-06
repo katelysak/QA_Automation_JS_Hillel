718332cd02b4f35d0ee4b21ec63d8c96488a35c1
Kate Lysak <lykaterina27@gmail.com>
2024-01-06 16:57:48 -0500

    My home work 2

---
+++ script1.js
@@ -0,0 +1,9 @@
+
+const myName = 'Kate';
+const myAge = 35;
+
+function personalInfo(value1, value2) {
+    return `My name is ${value1}. I am ${value2} years old.`;
+}
+
+console.log(personalInfo(myName, myAge));
---
+++ script2.js
@@ -0,0 +1,11 @@
+let name1 = 'Kate';
+
+console.log(name1);
+
+function printValue(passedValue) {
+    return `The value passed as parameter is ${passedValue}`;
+}
+
+let result = printValue('Test');
+
+console.log(result);
